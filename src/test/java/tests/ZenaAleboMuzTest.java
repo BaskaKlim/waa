405e817ca9bca2846b4b25e18eb8deabc81e0227
BaskaKlim <baska.klimekova@gmail.com>
2019-05-06 19:00:07 +0200

    domaca uloha z 30.4.

---
+++ src/test/java/tests/ZenaAleboMuzTest.java
@@ -0,0 +1,60 @@
+package tests;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+
+public class ZenaAleboMuzTest {
+    WebDriver driver;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver");
+        // 0. spustit prehliadac
+        driver = new ChromeDriver();
+        //1.otvorit stranku
+        driver.get("http://localhost:8888/zenaalebomuz.php");
+
+
+    }
+
+    @After
+    public void tearDown() {
+        driver.close();
+        driver.quit();
+        System.out.println("s panom bohom");
+    }
+
+    @Test
+    public void ItShouldBeNotSelected () {
+
+
+        Assert.assertFalse(driver.findElement(By.xpath("//input[@value='wurst']")).isSelected());
+        Assert.assertFalse(driver.findElement(By.xpath("//input[@value='conchita']")).isSelected());
+    }
+
+    @Test
+    public void ItShouldBeMan(){
+        driver.findElement(By.xpath("//input[@value='wurst']")).click();
+        Assert.assertTrue( "It's wurst", true);
+
+    }
+
+    @Test
+    public void ItShouldNotBeWoman(){
+        driver.findElement(By.xpath("//input[@value='wurst']")).click();
+        Assert.assertFalse(driver.findElement(By.xpath("//input[@value='conchita']")).isSelected());
+
+    }
+
+    @Test
+    public void ItShouldImageIsDisplayed (){
+        Assert.assertTrue(driver.findElement(By.cssSelector("img")).isDisplayed());
+
+    }
+}
