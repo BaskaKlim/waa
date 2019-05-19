2861cbe0011702c7073f8522019161eeeed52860
BaskaKlim <baska.klimekova@gmail.com>
2019-05-19 21:18:41 +0200

    vynatie casti before a after do samostatnych metod - XpathTrainingTest - definovanie si premennych - overovanie hodnot premennych

---
+++ src/test/java/tests/PreskusavanieSa/XpathTrainingTest.java
@@ -0,0 +1,68 @@
+package tests.PreskusavanieSa;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class XpathTrainingTest {
+    // zadefinujem si webdriver ktory je driver
+    WebDriver driver;
+    // zadefinujem si co sa stane pred spustenim kazdeho testu v tejto triede
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver");
+        //0.spustit prehliadac
+        driver = new ChromeDriver();
+        //1.otvorit stranku
+        driver.get("http://localhost:8888/xpathtrainingcenter.php");
+    }
+    // uadefinujem si co sa stane na konci kazdeho testu v tejto triedeł
+    @After
+    public void tearDown() {
+        driver.close();
+        driver.quit();
+    }
+
+    @Test
+    public void itShouldDisplayAction() {
+        // zadefinujem si premennu text z buttonu
+        String buttonText = "One more button";
+        // najdem element - text buttonu - cez lokator skrz xpath  a kliknem
+        driver.findElement(By.xpath("//button[contains(text(),'" + buttonText + "')]")).click();
+        // zadefinujem si premennu text spravy/hlasky
+        // najdem text hlasky pomocou css lokatora a vezmem do mapate text z neho
+
+        String actualMessage = driver.findElement(By.cssSelector("div.output h2 span")).getText();
+        // porovnam hlasku s tym, co cakam ze napise, bacha musim dat ocakavanu hlasku (moju premennu 2) do lowecase
+        Assert.assertEquals(
+                "you clicked " + buttonText.toLowerCase(),
+                actualMessage
+        );
+    }
+
+    @Test
+    public void itShouldDisplayEnteredMessage() {
+
+        // zadefinujem si premennu - vkladajucu hlasku
+        String message = "vonku prsi";
+        // najdem element - textove pole, kde sa pisu hlasky a vpisem hlasku
+        driver.findElement(By.cssSelector("input")).sendKeys(message);
+
+        // stlacim button hit me
+        driver.findElement(By.id("hitme")).click();
+
+        // zadefinujem si premennu zobrazenu hlasku - //precitam hodnotu zo stranky a ulozim ju do premennej
+        String actualMessage = driver.findElement(By.cssSelector("div.output h2 span")).getText();
+
+        // overim zobrazenu hlasku s vkladajucou hlaskou
+        Assert.assertEquals(
+                "you entered " + message,
+                actualMessage
+        );
+    }
+
+}
