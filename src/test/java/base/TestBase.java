c48a8db0c04eac7e404ad758be7060c30ad51018
BaskaKlim <baska.klimekova@gmail.com>
2019-05-20 07:25:49 +0200

    vytvorenie TestBase

---
+++ src/test/java/base/TestBase.java
@@ -0,0 +1,26 @@
+package base;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestBase {
+
+    protected WebDriver driver;
+    protected final String BASE_URL = "http://localhost:8888";
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver");
+        // 0. spustit prehliadac
+        driver = new ChromeDriver();
+    }
+    @After
+    public void tearDown() {
+        driver.close();
+        driver.quit();
+    }
+}
+
+//vytvorila som si test base pre vsetky packege
