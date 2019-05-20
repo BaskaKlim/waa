d3a6ccb53827d86960b1c928428ec24df50539c0
BaskaKlim <baska.klimekova@gmail.com>
2019-05-20 19:02:44 +0200

    Pokemon refaktorizacia pomocou metod a vstupnych parametrov

--- src/test/java/tests/preskusavanieSa/PokemonTest.java
+++ src/test/java/tests/preskusavanieSa/PokemonTest.java
@@ -24,23 +24,36 @@ public class PokemonTest extends TestBase {
         // zadefinujem si premmenu pole s moznostami
         String[] selectedPokemons = {"Pikachu", "Bulbasaur", "Charmander", "Diglett", "Geodude"};
 
-        //najdem element select
-        WebElement pokemonSelect = driver.findElement(By.cssSelector("select"));
-
         // cyklus for a podmienky
         // pre premennu pokemon z mojho definovaneho pola pokemonov
 
         for (String pokemon : selectedPokemons) {
-         //vyberiem pokemona
-            new Select(pokemonSelect).selectByVisibleText(pokemon);
-        //overim hlasku
+            //vyberiem pokemona  cez metodu co si zavolam
+            selectPokemon(pokemon);
+            //overim hlasku
             // zadefinuj si premennu aktualna hlaska, najdi ju a vloz do nej hodnotu
-            String actualMessage = driver.findElement(By.cssSelector("div.pokemon h3")).getText();
+            String actualMessage = getActualMessage();
             // zadefinuj si ocakavanu hlasku
             String expectedMessage = "I choose you " + pokemon + " !";
-            String expectedMessageByFormat = String.format("I choose you %s !", pokemon);
+            String expectedMessageByFormat = getExpectedlMessage(pokemon);
             //porovnaj
             Assert.assertEquals(expectedMessageByFormat, actualMessage);
         }
+    }
+
+    private void selectPokemon(String pokemonToSelect) {
+        // najprv najst tento element
+        WebElement pokemonSelect = driver.findElement(By.cssSelector("select"));
+        // vyber  element podla pokemona
+        new Select(pokemonSelect).selectByVisibleText(pokemonToSelect);
+    }
+
+    private String getActualMessage() {
+        return driver.findElement(By.cssSelector("div.pokemon h3")).getText();
+    }
+
+    private String getExpectedlMessage(String pokemonName) {
+        // metoda mi vrati ocakavanu hlasku "I choose you {{pokemon}}!
+        return String.format("I choose you %s !", pokemonName);
     }
 }
