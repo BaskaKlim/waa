cdda25639035f70391363f03afd9318081870a7b
BaskaKlim <baska.klimekova@gmail.com>
2019-05-26 22:39:57 +0200

    pokemon: refaktorizacia pomocou metod a vstupnych parametrov

--- src/test/java/tests/preskusavanieSa/PokemonTest.java
+++ src/test/java/tests/preskusavanieSa/PokemonTest.java
@@ -1,14 +1,11 @@
 package tests.preskusavanieSa;
 
 import base.TestBase;
-import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
 public class PokemonTest extends TestBase {
