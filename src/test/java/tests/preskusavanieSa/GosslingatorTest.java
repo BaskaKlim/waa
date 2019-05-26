ee664dbdb0792d4b8d23c4df019cd8da73317403
BaskaKlim <baska.klimekova@gmail.com>
2019-05-26 21:58:04 +0200

    refaktorizacia, vynatie vytvorenia GosslingatorPage do casti before

--- src/test/java/tests/preskusavanieSa/GosslingatorTest.java
+++ src/test/java/tests/preskusavanieSa/GosslingatorTest.java
@@ -9,24 +9,24 @@ import base.TestBase;
 import pages.GosslingatorPage;
 
 public class GosslingatorTest extends TestBase {
-
+    private GosslingatorPage gossPage;
     public String actualNumberOfRyans; //private = viditelna len pre triedu
 
     @Before
     public void openPage() {
         //1.otvorit stranku
         driver.get(BASE_URL + "/gosslingator.php");
+        gossPage = new GosslingatorPage(driver);
     }
 
     @Test
     public void itShouldAddOneRyan() {
-        GosslingatorPage gossPage = new GosslingatorPage(driver);
+
         //2.kliknut na tlacidlo pridat
         gossPage.addRyan();
         //3.overit pocitanie ryanov
         Assert.assertEquals("1", gossPage.getRyanCounterNumber());
-        // vypisem si do console aktualny pocet z pocitadla ryanov
-        Assert.assertEquals("ryan", gossPage.getCounterDescription());
+
     }
 
     @Test
@@ -49,39 +49,36 @@ public class GosslingatorTest extends TestBase {
 
     @Test
     public void itShouldDisplayWarningMessage() {
-        GosslingatorPage gossPage = new GosslingatorPage(driver);
-        //toto raz bude for cyklus
-        for (int i = 0; i < 50; i++) {
-            gossPage.addRyan();
-            //porovnam skutocnu hodnotu zo stranky s hodnotou indexu +1
-            //index si musim premenit na String aby som ich mohol porovnat
-            Assert.assertEquals(String.valueOf(i + 1), gossPage.getRyanCounterNumber());
-
-            //overit sklonovanie pomocou podmienky
-            if (i + 1 == 1) {
-                Assert.assertEquals("ryan", gossPage.getCounterDescription());
-            }
-            if (i + 1 >= 2) {
-                Assert.assertEquals("ryans", gossPage.getCounterDescription());
-            }
-            //overim pocet obrazkov ryana
-            Assert.assertEquals(i + 1, gossPage.getNumberOfRyanImages());
-            System.out.println("index i = " + i);
-            System.out.println("pocet ryanov = " + gossPage.getRyanCounterNumber());
-
-            Assert.assertEquals(
-                    "NUMBER OF\n" +
-                            "RYANS\n" +
-                            "IS TOO DAMN\n" +
-                            "HIGH",
-                    driver.findElement(By.cssSelector("h1.tooManyRyans")).getText()
-            );
+    GosslingatorPage gossPage = new GosslingatorPage(driver);
+    //toto raz bude for cyklus
+    for (int i = 0; i < 50; i++) {
+        gossPage.addRyan();
+        //porovnam skutocnu hodnotu zo stranky s hodnotou indexu +1
+        //index si musim premenit na String aby som ich mohol porovnat
+        Assert.assertEquals(String.valueOf(i + 1), gossPage.getRyanCounterNumber());
+        //overit sklonovanie pomocou podmienky
+        if (i + 1 == 1) {
+            Assert.assertEquals("ryan", gossPage.getCounterDescription());
         }
+        if (i + 1 >= 2) {
+            Assert.assertEquals("ryans", gossPage.getCounterDescription());
+        }
+        //overim pocet obrazkov ryana
+        Assert.assertEquals(i + 1, gossPage.getNumberOfRyanImages());
+        System.out.println("index i = " + i);
+        System.out.println("pocet ryanov = " + gossPage.getRyanCounterNumber());
     }
+    Assert.assertEquals(
+            "NUMBER OF\n" +
+            "RYANS\n" +
+            "IS TOO DAMN\n" +
+            "HIGH",
+            driver.findElement(By.cssSelector("h1.tooManyRyans")).getText()
+    );
+}
         @Test
         public void itShouldDisplayWarningMessageUsingWhileCycle () {
             //while cyklus sa vykona vzdy ak je podmienka "true"
-            GosslingatorPage gossPage = new GosslingatorPage(driver);
             int clicksLimit = 30;
             int clicks = 0;
 
@@ -94,7 +91,6 @@ public class GosslingatorTest extends TestBase {
         //  #2priklad: otestujte v cykle pocet zobrazenych obrazkov ryana.
         @Test
         public void itShouldDisplayNoRyanOnPageOpen () {
-            GosslingatorPage gossPage = new GosslingatorPage(driver);
             Assert.assertEquals(0, gossPage.getNumberOfRyanImages());
         }
     }
