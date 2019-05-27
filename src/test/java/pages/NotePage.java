558fb26d5330ea9c424f8c53d2202cfdd68db77e
BaskaKlim <baska.klimekova@gmail.com>
2019-05-27 18:19:46 +0200

    domaca uloha @findby pridane na stranke NotePage

---
+++ src/test/java/pages/NotePage.java
@@ -0,0 +1,57 @@
+pimport org.openqa.selenium.By;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class NotePage {
+
+    @FindBy(name = "title")
+    private WebElement titleInput;
+
+    @FindBy(name = "author")
+    private WebElement authorInput;
+
+    @FindBy(name = "message")
+    private WebElement messageInput;
+
+    @FindBy(css = "button.btn-block")
+    private WebElement submitButton;
+
+    private WebDriver pageDriver;
+
+    public NotePage(WebDriver driver) {
+        this.pageDriver = driver;
+        PageFactory.initElements(pageDriver, this);
+    }
+    public void enterNoteData(String title, String person, String message) {
+        titleInput.sendKeys(title);
+        authorInput.sendKeys(person);
+        messageInput.sendKeys(message);
+    }
+    public void submitNewNote() {
+        submitButton.click();
+    }
+
+    public WebElement getLastNoteFromList() {
+        return pageDriver.findElement(By.cssSelector("ul.list-of-sins > li:last-child"));
+    }
+    public void checkNoteDetail(String title, String author, String message) {
+        WebElement detail = pageDriver.findElement(By.cssSelector("div.content"));
+        Assert.assertEquals(title, detail.findElement(By.cssSelector("h4.title")).getText());
+        Assert.assertEquals(author, detail.findElement(By.cssSelector("h4.recipent")).getText());
+        Assert.assertEquals(message, detail.findElement(By.cssSelector("p")).getText());
+    }
+    public void checkNoteInList(String title) {
+        WebElement listItem = getLastNoteFromList();
+        //overim ze sa pridal novy zaznam do zoznamu
+        Assert.assertTrue(listItem.getText().contains(title));
+        //overenie linku
+        Assert.assertTrue(listItem.findElement(By.cssSelector("div.description a")).isDisplayed());
+        Assert.assertEquals("detail", listItem.findElement(By.cssSelector("div.description a")).getText());
+    }
+}
+
+}
