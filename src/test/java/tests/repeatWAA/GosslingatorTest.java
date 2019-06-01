37001b4c206951f7994674cd3d5d3e7f1ee78e0d
BaskaKlim <baska.klimekova@gmail.com>
2019-06-01 15:55:56 +0200

    repeatWAA - 3. Gosslingator test before a after

--- src/test/java/tests/repeatWAA/GosslingatorTest.java
+++ src/test/java/tests/repeatWAA/GosslingatorTest.java
@@ -1,45 +1,46 @@
 package tests.repeatWAA;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GosslingatorTest {
+    private WebDriver driver;
+    private String BASE_URL = "http://localhost:8888";
+
+    @Before
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "chromedriver");
+        driver = new ChromeDriver();
+        driver.get(BASE_URL + "/gosslingator.php");
+    }
 
 
     @Test
     public void itShoudlAddOneRyan() {
-        System.setProperty("webdriver.chrome.driver", "chromedriver");
-        //0.spustit prehliadac
-        WebDriver driver = new ChromeDriver();
-        //1.otvorit stranku
-        driver.get("http://localhost:8888/gosslingator.php");
+
         driver.findElement(By.id("addRyan")).click();
         Assert.assertEquals("1", driver.findElement(By.id("ryanCounter")).getText());
-        driver.close();
-        //ukoncit session
-        driver.quit();
     }
 
 
     @Test
     public void itShoudlAddTwoRyans() {
-        System.setProperty("webdriver.chrome.driver", "chromedriver");
-        //0.spustit prehliadac
-        WebDriver driver = new ChromeDriver();
-        //1.otvorit stranku
-        driver.get("http://localhost:8888/gosslingator.php");
         driver.findElement(By.id("addRyan")).click();
         driver.findElement(By.id("addRyan")).click();
         Assert.assertEquals("2", driver.findElement(By.id("ryanCounter")).getText());
-        driver.close();
+
+    }
+
+    @After
+    public void tearDown() {
         //ukoncit session
         driver.quit();
     }
-
-
 }
 
 
