ce5d6f7598223bec1caa9592bffbf09429cde4f8
BaskaKlim <baska.klimekova@gmail.com>
2019-06-01 23:56:41 +0200

    repeat WAA 7.GosslingatorTest vynatie buttonu addRyan do premennek

--- src/test/java/tests/repeatWAA/GosslingatorTest.java
+++ src/test/java/tests/repeatWAA/GosslingatorTest.java
@@ -6,6 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GosslingatorTest {
@@ -14,7 +15,7 @@ public class GosslingatorTest {
 
     @Before
     //urcim si driver a ovtorim stranku
-    public void setUp(){
+    public void setUp() {
         System.setProperty("webdriver.chrome.driver", "chromedriver");
         driver = new ChromeDriver();
         driver.get(BASE_URL + "/gosslingator.php");
@@ -23,97 +24,107 @@ public class GosslingatorTest {
     @Test
     //prida mi jedneho ryana
     public void itShoudlAddOneRyan() {
-    // najdem element button pridat Ryana a kliknem
-        driver.findElement(By.id("addRyan")).click();
-    // porovnam ci mi prida cislo na vysledku ake ocakavam, musim to dat do textu, porovnavam stringy
+        WebElement addRyanBtn = driver.findElement(By.id("addRyan"));
+        //pridanie premennej buttonu (najdem element button pridat Ryana a kliknem)
+
+        // klikknem na button
+        addRyanBtn.click();
+        // porovnam ci mi prida cislo na vysledku ake ocakavam, musim to dat do textu, porovnavam stringy
         Assert.assertEquals("1", driver.findElement(By.id("ryanCounter")).getText());
     }
 
     // prida mi 2 ryanov
     @Test
     public void itShoudlAddTwoRyans() {
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
+        WebElement addRyanBtn = driver.findElement(By.id("addRyan"));
+
+        addRyanBtn.click();
+        addRyanBtn.click();
         Assert.assertEquals("2", driver.findElement(By.id("ryanCounter")).getText());
 
     }
+
     // overim, ci mi spravne sklonuje ryan a ryans
     @Test
-    public void itShouldCheckDeclension(){
+    public void itShouldCheckDeclension() {
+        WebElement addRyanBtn = driver.findElement(By.id("addRyan"));
         // najdem element button pridat Ryana a kliknem
-        driver.findElement(By.id("addRyan")).click();
+        addRyanBtn.click();
         // porovnam ci mi prida cislo na vysledku ake ocakavam, musim to dat do textu, porovnavam stringy
         Assert.assertEquals("1", driver.findElement(By.id("ryanCounter")).getText());
-        Assert.assertEquals("ryan",driver.findElement(By.cssSelector("div.ryan-counter h3")).getText());
+        Assert.assertEquals("ryan", driver.findElement(By.cssSelector("div.ryan-counter h3")).getText());
         System.out.println("Number of ryan: " + driver.findElement(By.id("ryanCounter")).getText());
         // klikni este raz na button a over ci pribudlo spravne cislo aj sklonovanie
         driver.findElement(By.id("addRyan")).click();
         Assert.assertEquals("2", driver.findElement(By.id("ryanCounter")).getText());
-        Assert.assertEquals("ryans",driver.findElement(By.cssSelector("div.ryan-counter h3")).getText());
+        Assert.assertEquals("ryans", driver.findElement(By.cssSelector("div.ryan-counter h3")).getText());
         System.out.println("Number of ryans: " + driver.findElement(By.id("ryanCounter")).getText());
 
     }
     // overim, ci mi vyhodi hlasku ak dam too many ryans
 
     @Test
-    public void itShouldDisplayWarningMessage(){
-     // najdem element button pridat Ryana a kliknem
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
-        driver.findElement(By.id("addRyan")).click();
+    public void itShouldDisplayWarningMessage() {
+        // najdem element button pridat Ryana a kliknem
+        WebElement addRyanBtn = driver.findElement(By.id("addRyan"));
+        addRyanBtn.click();
+        addRyanBtn.click();
+        addRyanBtn.click();
+        addRyanBtn.click();
+        addRyanBtn.click();
+        addRyanBtn.click();
+        addRyanBtn.click();
+        addRyanBtn.click();
+        addRyanBtn.click();
+        addRyanBtn.click();
+        addRyanBtn.click();
+        addRyanBtn.click();
+        addRyanBtn.click();
+        addRyanBtn.click();
+        addRyanBtn.click();
+        addRyanBtn.click();
+        addRyanBtn.click();
+        addRyanBtn.click();
+        addRyanBtn.click();
+        addRyanBtn.click();
+        addRyanBtn.click();
+        addRyanBtn.click();
+        addRyanBtn.click();
+        addRyanBtn.click();
+        addRyanBtn.click();
+        addRyanBtn.click();
+        addRyanBtn.click();
+        addRyanBtn.click();
+        addRyanBtn.click();
+        addRyanBtn.click();
+        addRyanBtn.click();
+        addRyanBtn.click();
+        addRyanBtn.click();
+        addRyanBtn.click();
+        addRyanBtn.click();
+        addRyanBtn.click();
+        addRyanBtn.click();
+        addRyanBtn.click();
+        addRyanBtn.click();
+        addRyanBtn.click();
+        addRyanBtn.click();
+        addRyanBtn.click();
+        addRyanBtn.click();
+        addRyanBtn.click();
+        addRyanBtn.click();
+        addRyanBtn.click();
+        addRyanBtn.click();
+        addRyanBtn.click();
+        addRyanBtn.click();
+        addRyanBtn.click();
+        addRyanBtn.click();
+
 
 
         Assert.assertEquals("NUMBER OF\n" +
-                "RYANS\n" +
-                "IS TOO DAMN\n" +
-                "HIGH",
+                        "RYANS\n" +
+                        "IS TOO DAMN\n" +
+                        "HIGH",
                 driver.findElement(By.cssSelector("h1.tooManyRyans.ryan-title")).getText());
 
 
@@ -123,9 +134,10 @@ public class GosslingatorTest {
     public void itShouldDisplayTitle() {
         driver.findElement(By.cssSelector("h1.ryan-title"));
         Assert.assertEquals("GOSLINGATE ME", driver.findElement(By.cssSelector("h1.ryan-title")).getText());
-        System.out.println("Title is:"+ driver.findElement(By.cssSelector("h1.ryan-title")).getText());
+        System.out.println("Title is:" + driver.findElement(By.cssSelector("h1.ryan-title")).getText());
 
     }
+
     @After
     public void tearDown() {
         //ukoncit session
