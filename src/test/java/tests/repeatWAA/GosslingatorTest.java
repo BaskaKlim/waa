20e2fb397aa3b33b89f0d5c9ca4d4b988e2626ef
BaskaKlim <baska.klimekova@gmail.com>
2019-06-01 17:26:14 +0200

    repeat WAA
    4.Gosslingator test
    porovnanie  sklonovania ryan a ryans
    prikaz System.out.println - vypisat hlasku

--- src/test/java/tests/repeatWAA/GosslingatorTest.java
+++ src/test/java/tests/repeatWAA/GosslingatorTest.java
@@ -13,26 +13,44 @@ public class GosslingatorTest {
     private String BASE_URL = "http://localhost:8888";
 
     @Before
+    //urcim si driver a ovtorim stranku
     public void setUp(){
         System.setProperty("webdriver.chrome.driver", "chromedriver");
         driver = new ChromeDriver();
         driver.get(BASE_URL + "/gosslingator.php");
     }
 
-
     @Test
+    //prida mi jedneho ryana
     public void itShoudlAddOneRyan() {
-
+    // najdem element button pridat Ryana a kliknem
         driver.findElement(By.id("addRyan")).click();
+    // porovnam ci mi prida cislo na vysledku ake ocakavam, musim to dat do textu, porovnavam stringy
         Assert.assertEquals("1", driver.findElement(By.id("ryanCounter")).getText());
     }
 
-
+    // prida mi 2 ryanov
     @Test
     public void itShoudlAddTwoRyans() {
         driver.findElement(By.id("addRyan")).click();
         driver.findElement(By.id("addRyan")).click();
         Assert.assertEquals("2", driver.findElement(By.id("ryanCounter")).getText());
+
+    }
+    // overim, ci mi spravne sklonuje ryan a ryans
+    @Test
+    public void itShouldCheckDeclension(){
+        // najdem element button pridat Ryana a kliknem
+        driver.findElement(By.id("addRyan")).click();
+        // porovnam ci mi prida cislo na vysledku ake ocakavam, musim to dat do textu, porovnavam stringy
+        Assert.assertEquals("1", driver.findElement(By.id("ryanCounter")).getText());
+        Assert.assertEquals("ryan",driver.findElement(By.cssSelector("div.ryan-counter h3")).getText());
+        System.out.println("Number of ryan: " + driver.findElement(By.id("ryanCounter")).getText());
+        // klikni este raz na button a over ci pribudlo spravne cislo aj sklonovanie
+        driver.findElement(By.id("addRyan")).click();
+        Assert.assertEquals("2", driver.findElement(By.id("ryanCounter")).getText());
+        Assert.assertEquals("ryans",driver.findElement(By.cssSelector("div.ryan-counter h3")).getText());
+        System.out.println("Number of ryans: " + driver.findElement(By.id("ryanCounter")).getText());
 
     }
 
