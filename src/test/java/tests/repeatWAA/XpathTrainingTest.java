31d4f78c05b402ed32241e9776e6c5d0b1c1e87f
BaskaKlim <baska.klimekova@gmail.com>
2019-06-02 22:23:59 +0200

    repeat WAA 3. XpathTrainingTest domaca uloha 1.test ktory prejde cez kazdy button, klikne nan a overi hlasku - vyuzite `pole` a `iteraciu` cez toto pole - neslo to! 2.test ktory vyberie kazdu moznost zo selectu a overi hlasku - vyuzite `for cyklus` - ISLO 3. test ktory vyberie kazdu moznost zo selectu c.2, obdobne ako v predch, priklade - ISLO

--- src/test/java/tests/repeatWAA/XpathTrainingTest.java
+++ src/test/java/tests/repeatWAA/XpathTrainingTest.java
@@ -6,7 +6,12 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import javax.swing.*;
+import javax.xml.parsers.SAXParser;
 
 public class XpathTrainingTest {
 
@@ -32,7 +37,7 @@ public class XpathTrainingTest {
 
 
     @Test
-    public void itShouldDisplayActionByDefineVariables(){
+    public void itShouldDisplayActionByDefineVariables() {
         // zadefinovnie si premennych
 
         String buttonText = "One more button";
@@ -52,7 +57,7 @@ public class XpathTrainingTest {
 
     public void itShouldDisplayEnteredMessage() {
         //1. zadefinovanie si premennej - entered message
-        String enteredMessage ="vonku prsi";
+        String enteredMessage = "vonku prsi";
         // 2. najdi element textoveho pola         // 3. vpis don premennu
         driver.findElement(By.cssSelector("input.form-control")).sendKeys(enteredMessage);
         //3. click hit mebutton
@@ -60,6 +65,48 @@ public class XpathTrainingTest {
         //4. over pozadovanu hlasku so skutocnou
         Assert.assertEquals(driver.findElement(By.xpath("//h2/span")).getText(), "you entered " + enteredMessage);
     }
+
+    // DOMACA ULOHA test ktory prejde cez kazdy button, klikne nan a overi hlasku - vyuzite `pole` a `iteraciu` cez toto pole
+
+    // DOMACA ULOHA test ktory vyberie kazdu moznost zo selectu a overi hlasku - vyuzite `for cyklus`
+    @Test
+
+    public void itShouldChoosEachOption() {
+        // definujem si pole s moznostami
+        String[] selectedOptions = {"Moznost 1","Moznost 2", "Moznost 3", "Moznost 4", "Moznost 5", "Moznost 6"};
+        //najdem si tento select element
+      WebElement select = driver.findElement(By.cssSelector("div > select:nth-child(1)"));
+        //vyberem moznost  z tohto selektora
+
+        for (String option :selectedOptions) {
+           new Select(select).selectByVisibleText(option);
+
+            //overim hlasku
+            String actualMessage = driver.findElement(By.cssSelector(" div > h2 > span")).getText();
+            String expectedMessage = "you have chosen " + option.toLowerCase();
+            Assert.assertEquals(expectedMessage,actualMessage);
+        }
+        }
+    // DOMACA ULOHA test ktory vyberie kazdu moznost zo selectu c.2, obdobne ako v predch, priklade
+    @Test
+    public void itShouldChoosEachOption2() {
+        // definujem si pole s moznostami
+        String[] selectedOptions2 = {"Option 1", "Option 2", "Option 3", "Option 4", "Option 5", "Option 6"};
+        //najdem si tento select element
+        WebElement select = driver.findElement(By.cssSelector("div > select:nth-child(2)"));
+        //vyberem moznost  z tohto selektora
+
+        for (String option2 :selectedOptions2) {
+            new Select(select).selectByVisibleText(option2);
+
+            //overim hlasku
+            String actualMessage = driver.findElement(By.cssSelector(" div > h2 > span")).getText();
+            String expectedMessage = "you have chosen " + option2.toLowerCase();
+            Assert.assertEquals(expectedMessage,actualMessage);
+        }
+    }
+
+
     @After
     public void tearDown() {
         //ukoncit session
